fe7f987feb5844b3e027f4b5f5f1cc961e870842
Matthieu Collet <matthieucollet25@gmail.com>
2023-01-17 10:02:21 +0100

    [INIT]

---
+++ WPFClient/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WPFClient
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ WPFClient/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace WPFClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
